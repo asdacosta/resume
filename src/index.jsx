be2c2518358671065a435e8d583f9a1d15cb69cc
asdacosta <asdacosta18@gmail.com>
2024-03-29 21:57:30 +0000

    Rename file

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,10 @@
-import "./styles/index.css";
-import "./styles/reset.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// import './index.css'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
