0c38441906e3a2a9b357794e2878b8b99993597f
asdacosta <asdacosta18@gmail.com>
2024-03-31 18:15:28 +0000

    Animate domain name of placeholders

--- src/components/animateText.js
+++ src/components/animateText.js
@@ -22,10 +22,6 @@ const animateText = function () {
       const domainNames = ["yahoo", "outlook", "icloud", "aol", "university.edu"];
       const placeholder = getNodes().emailInput.placeholder;
 
-      //   for (let charIndex = 0; charIndex < localNames[0].length; charIndex++) {
-      //     getNodes().emailInput.placeholder -= placeholder[charIndex];
-      //   }
-
       const animateLocalName = function (localIndex) {
         const append = async function () {
           for (const char of localNames[localIndex]) {
@@ -58,22 +54,71 @@ const animateText = function () {
         })();
       };
 
+      const animateDomainName = function (localIndex) {
+        const append = async function () {
+          for (const char of domainNames[localIndex]) {
+            let alteredPlaceholder = getNodes().emailInput.placeholder;
+            await new Promise((resolve) => {
+              setTimeout(resolve, 150);
+            });
+            const strArray = alteredPlaceholder.split("");
+            strArray.splice(strArray.indexOf("."), 0, char);
+            const updatedStr = strArray.join("");
+            getNodes().emailInput.placeholder = updatedStr;
+          }
+          await new Promise((resolve) => {
+            setTimeout(resolve, 500);
+          });
+        };
+
+        const removeAppended = (async function () {
+          await append();
+          for (const char of domainNames[localIndex]) {
+            let alteredPlaceholder = getNodes().emailInput.placeholder;
+            await new Promise((resolve) => {
+              setTimeout(resolve, 150);
+            });
+            const strArray = alteredPlaceholder.split("");
+            strArray.splice(strArray.indexOf(".") - 1, 1);
+            const updatedStr = strArray.join("");
+            getNodes().emailInput.placeholder = updatedStr;
+          }
+        })();
+      };
+
       const animateInOrder = (async function () {
+        // await new Promise((resolve) => {
+        //   animateLocalName(0);
+        //   setTimeout(resolve, 2600);
+        // });
+        // await new Promise((resolve) => {
+        //   animateLocalName(1);
+        //   setTimeout(resolve, 2600);
+        // });
+        // await new Promise((resolve) => {
+        //   animateLocalName(2);
+        //   setTimeout(resolve, 2600);
+        // });
+        // await new Promise((resolve) => {
+        //   animateLocalName(3);
+        //   setTimeout(resolve, 2600);
+        // });
+
         await new Promise((resolve) => {
-          animateLocalName(0);
+          animateDomainName(0);
           setTimeout(resolve, 2600);
         });
         await new Promise((resolve) => {
-          animateLocalName(1);
+          animateDomainName(1);
+          setTimeout(resolve, 2800);
+        });
+        await new Promise((resolve) => {
+          animateDomainName(2);
           setTimeout(resolve, 2600);
         });
         await new Promise((resolve) => {
-          animateLocalName(2);
-          setTimeout(resolve, 2600);
-        });
-        await new Promise((resolve) => {
-          animateLocalName(3);
-          setTimeout(resolve, 2600);
+          animateDomainName(3);
+          setTimeout(resolve, 2200);
         });
       })();
     })();
