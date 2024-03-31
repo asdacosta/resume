d36de1c757793d6194287796dece644ebfe7a00d
asdacosta <asdacosta18@gmail.com>
2024-03-31 13:31:26 +0000

    Add html boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./styles/App.css";
+// import "./styles/reset.css";
 
 function App() {
   return (
@@ -7,41 +8,183 @@ function App() {
       <section className="edit-cover">
         <nav>
           <section className="menu">
-            <div></div>
-            <div></div>
-            <div></div>
+            <i class="fa-solid fa-circle"></i>
+            <i class="fa-solid fa-caret-down"></i>
+            <i class="fa-solid fa-circle"></i>
           </section>
           <section className="sample-bar">
             <span>Clear</span>
-            {/* <i className="fa-solid fa-gear"></i> */}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <title>Wheel</title>
-              <path d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M12,10A2,2 0 0,0 10,12A2,2 0 0,0 12,14A2,2 0 0,0 14,12A2,2 0 0,0 12,10M10,22C9.75,22 9.54,21.82 9.5,21.58L9.13,18.93C8.5,18.68 7.96,18.34 7.44,17.94L4.95,18.95C4.73,19.03 4.46,18.95 4.34,18.73L2.34,15.27C2.21,15.05 2.27,14.78 2.46,14.63L4.57,12.97L4.5,12L4.57,11L2.46,9.37C2.27,9.22 2.21,8.95 2.34,8.73L4.34,5.27C4.46,5.05 4.73,4.96 4.95,5.05L7.44,6.05C7.96,5.66 8.5,5.32 9.13,5.07L9.5,2.42C9.54,2.18 9.75,2 10,2H14C14.25,2 14.46,2.18 14.5,2.42L14.87,5.07C15.5,5.32 16.04,5.66 16.56,6.05L19.05,5.05C19.27,4.96 19.54,5.05 19.66,5.27L21.66,8.73C21.79,8.95 21.73,9.22 21.54,9.37L19.43,11L19.5,12L19.43,13L21.54,14.63C21.73,14.78 21.79,15.05 21.66,15.27L19.66,18.73C19.54,18.95 19.27,19.04 19.05,18.95L16.56,17.95C16.04,18.34 15.5,18.68 14.87,18.93L14.5,21.58C14.46,21.82 14.25,22 14,22H10M11.25,4L10.88,6.61C9.68,6.86 8.62,7.5 7.85,8.39L5.44,7.35L4.69,8.65L6.8,10.2C6.4,11.37 6.4,12.64 6.8,13.8L4.68,15.36L5.43,16.66L7.86,15.62C8.63,16.5 9.68,17.14 10.87,17.38L11.24,20H12.76L13.13,17.39C14.32,17.14 15.37,16.5 16.14,15.62L18.57,16.66L19.32,15.36L17.2,13.81C17.6,12.64 17.6,11.37 17.2,10.2L19.31,8.65L18.56,7.35L16.15,8.39C15.38,7.5 14.32,6.86 13.12,6.62L12.75,4H11.25Z" />
-            </svg>
+            <i className="fa-solid fa-gear"></i>
           </section>
           <section className="download">
             <i class="fa-solid fa-arrow-down"></i>
           </section>
           <section className="view">
-            <i class="fa-solid fa-eye"></i>
             <span></span>
+            <i class="fa-solid fa-eye"></i>
           </section>
         </nav>
         <section className="input-fields">
           <section className="personal-info">
-            <p>Personal Info</p>
+            <p>
+              <i class="fa-solid fa-user-large"></i>
+              Personal Info
+              <span className="open-field">
+                <i class="fa-solid fa-arrow-right fa-rotate-270"></i>
+                <i class="fa-solid fa-arrow-right fa-rotate-90"></i>
+              </span>
+            </p>
+            <section>
+              <div className="field">
+                <label for="first-names">First Name(s)</label>
+                <input type="text" name="first-names" id="first-names" />
+              </div>
+              <div className="field">
+                <label for="last-name">Last Name</label>
+                <input type="text" name="last-name" id="last-name" />
+              </div>
+              <div className="field">
+                <label for="mail">Email</label>
+                <input type="email" name="mail" id="mail" />
+              </div>
+              <div className="field">
+                <label for="phone">Phone number</label>
+                <input type="tel" name="last-name" id="last-name" />
+              </div>
+              <div className="field">
+                <label for="country">Country</label>
+                <input type="text" name="country" id="country" />
+              </div>
+              <div className="field">
+                <label for="city">City</label>
+                <input type="text" name="city" id="city" />
+              </div>
+              <div className="field">
+                <label for="address">Address</label>
+                <input type="text" name="last-name" id="last-name" />
+              </div>
+              <div className="field-profile">
+                <label for="profile">
+                  <i class="fa-solid fa-images fa-fade"></i>Upload Picture
+                </label>
+                <input type="file" name="profile" id="profile" accept="image/*" />
+              </div>
+            </section>
           </section>
           <section className="education">
-            <p>Education</p>
+            <p>
+              <i class="fa-solid fa-user-graduate"></i>
+              Education
+              <span className="open-field">
+                <i class="fa-solid fa-arrow-right fa-rotate-270"></i>
+                <i class="fa-solid fa-arrow-right fa-rotate-90"></i>
+              </span>
+            </p>
+            <div className="add">
+              <span>
+                <i class="fa-solid fa-plus"></i> Education
+              </span>
+            </div>
+            <section>
+              <div className="field">
+                <label for="school">School</label>
+                <input type="text" name="school" id="school" />
+              </div>
+              <div className="field">
+                <label for="degree">Degree</label>
+                <input type="text" name="degree" id="degree" />
+              </div>
+              <div className="field">
+                <label for="honors">Class Honors</label>
+                <input type="text" name="honors" id="honors" />
+              </div>
+              <div className="field">
+                <label for="start-date">Start Date</label>
+                <input type="date" name="start-date" id="start-date" />
+              </div>
+              <div className="field">
+                <label for="end-date">End Date</label>
+                <input type="date" name="end-date" id="end-date" />
+              </div>
+              <div className="field">
+                <label for="country">Country</label>
+                <input type="text" name="country" id="country" />
+              </div>
+              <div className="field">
+                <label for="city">City</label>
+                <input type="text" name="city" id="city" />
+              </div>
+              <div className="field-footer">
+                <span>
+                  <i class="fa-solid fa-eraser"></i> Clear
+                </span>
+                <span>
+                  <i class="fa-solid fa-pencil"></i> Add
+                </span>
+              </div>
+            </section>
           </section>
           <section className="professional">
-            <p>Professional Experience</p>
+            <p>
+              <i class="fa-solid fa-user-tie"></i>
+              Professional Experience
+              <span className="open-field">
+                <i class="fa-solid fa-arrow-right fa-rotate-270"></i>
+                <i class="fa-solid fa-arrow-right fa-rotate-90"></i>
+              </span>
+            </p>
+            <div className="add">
+              <span>
+                <i class="fa-solid fa-plus"></i> Experience
+              </span>
+            </div>
+            <section>
+              <div className="field">
+                <label for="company">Company Name</label>
+                <input type="text" name="company" id="company" />
+              </div>
+              <div className="field">
+                <label for="position">Position</label>
+                <input type="text" name="position" id="position" />
+              </div>
+              <div className="field">
+                <label for="start-date">Start Date</label>
+                <input type="date" name="start-date" id="start-date" />
+              </div>
+              <div className="field">
+                <label for="end-date">End Date</label>
+                <input type="date" name="end-date" id="end-date" />
+              </div>
+              <div className="field">
+                <label for="country">Country</label>
+                <input type="text" name="country" id="country" />
+              </div>
+              <div className="field">
+                <label for="city">City</label>
+                <input type="text" name="city" id="city" />
+              </div>
+              <div className="field-description">
+                <label for="description">Description</label>
+                <textarea
+                  name="description"
+                  id="description"
+                  cols="20"
+                  rows="10"
+                ></textarea>
+              </div>
+              <div className="field-footer">
+                <span>
+                  <i class="fa-solid fa-eraser"></i> Clear
+                </span>
+                <span>
+                  <i class="fa-solid fa-pencil"></i> Add
+                </span>
+              </div>
+            </section>
           </section>
         </section>
       </section>
-      <section className="resume-cover">
-        <p>Ever Ace</p>
-      </section>
+      <section className="resume-cover"></section>
     </>
   );
 }
