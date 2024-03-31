e312379969ad70712c035889083db6a02263a6ee
asdacosta <asdacosta18@gmail.com>
2024-03-31 18:16:23 +0000

    Select inputs with placeholders

--- src/components/getNodes.js
+++ src/components/getNodes.js
@@ -1,8 +1,22 @@
 const getNodes = function () {
   const fieldStatus = document.querySelectorAll(".optional, .required");
+  const emailInput = document.querySelector("#mail");
+  const phoneInput = document.querySelector("#phone");
+  const addressInput = document.querySelector("#address");
+  const degreeInput = document.querySelector("#degree");
+  const honorsInput = document.querySelector("#honors");
+  const positionInput = document.querySelector("#position");
+  const descriptionInput = document.querySelector("#description");
 
   return {
     fieldStatus,
+    emailInput,
+    phoneInput,
+    addressInput,
+    degreeInput,
+    honorsInput,
+    positionInput,
+    descriptionInput,
   };
 };
 
