6a1ec090b3b935cc29ef6af98f96801bada433ce
asdacosta <asdacosta18@gmail.com>
2024-04-01 21:21:32 +0000

    Choose default personalization

--- src/components/nav.js
+++ src/components/nav.js
@@ -20,6 +20,43 @@ const navigation = function () {
         caret.style.transform = "translateY(0.5rem)";
       });
     })();
+
+    const __dialog = (function () {
+      const chooseDefault = (function () {
+        getNodes().paletteButtons[0].classList.add("chosen");
+        getNodes().layoutButtons[0].classList.add("chosen");
+      })();
+
+      const choosePalette = (function () {
+        const palletteButtons = getNodes().paletteButtons;
+        palletteButtons.forEach((button) => {
+          button.addEventListener("click", () => {
+            const removePreviousChosen = (function () {
+              palletteButtons.forEach((preButton) => {
+                preButton.classList.remove("chosen");
+              });
+            })();
+            button.classList.add("chosen");
+          });
+        });
+      })();
+
+      const chooseLayout = (function () {
+        const layoutButtons = getNodes().layoutButtons;
+        layoutButtons.forEach((button) => {
+          button.addEventListener("click", () => {
+            const removePreviousChosen = (function () {
+              layoutButtons.forEach((preButton) => {
+                preButton.classList.remove("chosen");
+              });
+            })();
+            button.classList.add("chosen");
+          });
+        });
+      })();
+    })();
+
+    const __sampleBar = (function () {})();
   })();
 };
 
