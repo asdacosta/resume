6178294e0fcf58d18f9eaaf6371942c9f878f8d5
asdacosta <asdacosta18@gmail.com>
2024-04-01 01:19:58 +0000

    Add animation of phone placeholder

--- src/components/animateText.js
+++ src/components/animateText.js
@@ -16,118 +16,148 @@ const animateText = function () {
     });
   })();
 
-  const animatePlaceholders = (function () {
-    const animateEmailField = (async function () {
-      const localNames = [".green", "-green", "_green", "2024"];
-      const domainNames = [
-        "example",
-        "yahoo",
-        "outlook",
-        "icloud",
-        "aol",
-        "university.edu",
-        "example.com",
-      ];
+  const animatePlaceholders = (async function () {
+    // Email Samples
+    const localNames = [".green", "-green", "_green", "2024"];
+    const domainNames = [
+      "example",
+      "yahoo",
+      "outlook",
+      "icloud",
+      "aol",
+      "university.edu",
+      "example.com",
+    ];
 
-      const animateMail = function (
-        names,
-        localIndex,
+    // Phone Samples
+    const phoneSamples = [
+      "-●●●-●●●-●●●●",
+      " ●●● ●●● ●●●●",
+      "●●●●●●●●●●",
+      "-●●●-●●●-●●●●",
+    ];
+
+    const animateInput = function (
+      node,
+      names,
+      localIndex,
+      stopChar,
+      stopCharIsLastIndex = true,
+    ) {
+      let name = names[localIndex];
+
+      const append = async function () {
+        if (name === "example" || (names === phoneSamples && localIndex === 0)) {
+          // Don't append for initial case, just erase;
+          return;
+        }
+
+        for (const char of name) {
+          let alteredPlaceholder = node.placeholder;
+          await new Promise((resolve) => {
+            setTimeout(resolve, 150);
+          });
+
+          const appendAndUpdate = (function () {
+            const strArray = alteredPlaceholder.split("");
+            if (stopCharIsLastIndex === true) {
+              strArray.splice(strArray.lastIndexOf(stopChar), 0, char);
+            } else {
+              strArray.splice(strArray.length, 0, char);
+            }
+            const updatedStr = strArray.join("");
+            node.placeholder = updatedStr;
+          })();
+        }
+        await new Promise((resolve) => {
+          setTimeout(resolve, 500);
+        });
+      };
+
+      const erase = (async function () {
+        await append();
+        if (name === "example.com" || (names === phoneSamples && localIndex === 3)) {
+          // Append and stop
+          return;
+        }
+        const clearAllForLastDomain = (function () {
+          if (names === domainNames && localIndex === 4) {
+            name = "aol.com";
+            stopCharIsLastIndex = false;
+          }
+        })();
+
+        for (const char of name) {
+          let alteredPlaceholder = node.placeholder;
+          await new Promise((resolve) => {
+            setTimeout(resolve, 150);
+          });
+
+          const eraseAndUpdate = (function () {
+            const strArray = alteredPlaceholder.split("");
+            if (stopCharIsLastIndex === true) {
+              strArray.splice(strArray.lastIndexOf(stopChar) - 1, 1);
+            } else {
+              strArray.splice(strArray.length - 1, 1);
+            }
+            const updatedStr = strArray.join("");
+            node.placeholder = updatedStr;
+          })();
+        }
+      })();
+    };
+
+    const animateMail = (async function () {
+      const node = getNodes().emailInput;
+
+      const __localNames = async function () {
+        for (let index = 0; index < 4; index++) {
+          await new Promise((resolve) => {
+            animateInput(node, localNames, index, "@");
+            setTimeout(resolve, 2600);
+          });
+        }
+      };
+      await __localNames();
+
+      const __domainNames = async function (
+        index,
+        timer,
         stopChar,
         stopCharIsLastIndex = true,
       ) {
-        let name = names[localIndex];
-
-        const append = async function () {
-          if (name === "example") {
-            // Don't append, just erase;
-            return;
-          }
-
-          for (const char of name) {
-            let alteredPlaceholder = getNodes().emailInput.placeholder;
-            await new Promise((resolve) => {
-              setTimeout(resolve, 150);
-            });
-
-            const appendAndUpdate = (function () {
-              const strArray = alteredPlaceholder.split("");
-              if (stopCharIsLastIndex === true) {
-                strArray.splice(strArray.lastIndexOf(stopChar), 0, char);
-              } else {
-                strArray.splice(strArray.length, 0, char);
-              }
-              const updatedStr = strArray.join("");
-              getNodes().emailInput.placeholder = updatedStr;
-            })();
-          }
-          await new Promise((resolve) => {
-            setTimeout(resolve, 500);
-          });
-        };
-
-        const erase = (async function () {
-          await append();
-          if (name === "example.com") {
-            // Append and stop
-            return;
-          }
-          const clearAllForLastDomain = (function () {
-            if (names === domainNames && localIndex === 4) {
-              name = "aol.com";
-              stopCharIsLastIndex = false;
-            }
-          })();
-
-          for (const char of name) {
-            let alteredPlaceholder = getNodes().emailInput.placeholder;
-            await new Promise((resolve) => {
-              setTimeout(resolve, 150);
-            });
-
-            const eraseAndUpdate = (function () {
-              const strArray = alteredPlaceholder.split("");
-              if (stopCharIsLastIndex === true) {
-                strArray.splice(strArray.lastIndexOf(stopChar) - 1, 1);
-              } else {
-                strArray.splice(strArray.length - 1, 1);
-              }
-              const updatedStr = strArray.join("");
-              getNodes().emailInput.placeholder = updatedStr;
-            })();
-          }
-        })();
+        await new Promise((resolve) => {
+          animateInput(node, domainNames, index, stopChar, stopCharIsLastIndex);
+          setTimeout(resolve, timer);
+        });
       };
+      await __domainNames(0, 1400, ".");
+      await __domainNames(1, 2400, ".");
+      await __domainNames(2, 2800, ".");
+      await __domainNames(3, 2600, ".");
+      await __domainNames(4, 2600, ".");
+      await __domainNames(5, 5000, "@", false);
+      await __domainNames(6, 2600, "@", false);
+    })();
 
-      const animateInOrder = (async function () {
-        const __localNames = async function () {
-          for (let index = 0; index < 4; index++) {
-            await new Promise((resolve) => {
-              animateMail(localNames, index, "@");
-              setTimeout(resolve, 2600);
-            });
-          }
-        };
-        await __localNames();
+    const animatePhoneField = (async function () {
+      const node = getNodes().phoneInput;
 
-        const __domainNames = async function (
-          index,
-          timer,
-          stopChar,
-          stopCharIsLastIndex = true,
-        ) {
-          await new Promise((resolve) => {
-            animateMail(domainNames, index, stopChar, stopCharIsLastIndex);
-            setTimeout(resolve, timer);
-          });
-        };
-        await __domainNames(0, 1400, ".");
-        await __domainNames(1, 2400, ".");
-        await __domainNames(2, 2800, ".");
-        await __domainNames(3, 2600, ".");
-        await __domainNames(4, 2800, ".");
-        await __domainNames(5, 5000, "@", false);
-        await __domainNames(6, 2600, "@", false);
-      })();
+      const __allSamples = async function (
+        index,
+        timer,
+        stopChar,
+        stopCharIsLastIndex = false,
+      ) {
+        await new Promise((resolve) => {
+          animateInput(node, phoneSamples, index, stopChar, stopCharIsLastIndex);
+          setTimeout(resolve, timer);
+        });
+      };
+      await __allSamples(0, 2500, "●");
+      await __allSamples(1, 4500, "●");
+      await __allSamples(2, 4500, "●");
+      await __allSamples(3, 2500, "●");
     })();
   })();
 };
