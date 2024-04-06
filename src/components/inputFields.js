49e890b993f4e1777c8493d2877caf1fa0fb46a9
asdacosta <asdacosta18@gmail.com>
2024-04-06 02:13:50 +0000

    Alter shape of locker when opening and closing

--- src/components/inputFields.js
+++ src/components/inputFields.js
@@ -10,6 +10,55 @@ const inputFields = function () {
       uploadBox.querySelector("i").classList.add("fa-fade");
     });
   })();
+
+  const displayAddButton = (function () {
+    let educationExpanded = false;
+    let professionalExpanded = false;
+
+    const __forEducation = (function () {
+      getNodes().educationParagraph.addEventListener("click", async () => {
+        // Close if opened
+        if (educationExpanded) {
+          getNodes().educationLocker.style.transform = "rotate(45deg)";
+          await new Promise((resolve) => {
+            setTimeout(resolve, 800);
+          });
+          getNodes().educationLocker.style.gap = "0";
+          educationExpanded = false;
+          return;
+        }
+        // Open
+        getNodes().educationLocker.style.gap = "0.5rem";
+        await new Promise((resolve) => {
+          setTimeout(resolve, 500);
+        });
+        getNodes().educationLocker.style.transform = "rotate(0)";
+        educationExpanded = true;
+      });
+    })();
+
+    const __forProfessional = (function () {
+      getNodes().professionalParagraph.addEventListener("click", async () => {
+        // Close if opened
+        if (professionalExpanded) {
+          getNodes().professionalLocker.style.transform = "rotate(45deg)";
+          await new Promise((resolve) => {
+            setTimeout(resolve, 800);
+          });
+          getNodes().professionalLocker.style.gap = "0";
+          professionalExpanded = false;
+          return;
+        }
+        // Open
+        getNodes().professionalLocker.style.gap = "0.5rem";
+        await new Promise((resolve) => {
+          setTimeout(resolve, 500);
+        });
+        getNodes().professionalLocker.style.transform = "rotate(0)";
+        professionalExpanded = true;
+      });
+    })();
+  })();
 };
 
 export { inputFields };
