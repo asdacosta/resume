ccc8b4af76e0fe7316e4931b249e4141ce64a5f1
asdacosta <asdacosta18@gmail.com>
2024-04-09 20:54:03 +0000

    Automate and include professional field in defined functions

--- src/components/responsiveTyping.js
+++ src/components/responsiveTyping.js
@@ -1,12 +1,18 @@
 import { getNodes } from "./getNodes";
 
 const responsiveTyping = function () {
+  const __forIndepInput = function (inputTake, inputGive) {
+    inputGive.addEventListener("input", () => {
+      inputTake.textContent = inputGive.value;
+    });
+  };
+
   const __forPersonalInfo = (function () {
     function __forNames(input) {
       input.addEventListener("input", () => {
-        let firstNames = getNodes().firstNamesInput.value;
-        let lastName = getNodes().lastNameInput.value;
-        let fullName = firstNames + " " + lastName;
+        const firstNames = getNodes().firstNamesInput.value;
+        const lastName = getNodes().lastNameInput.value;
+        const fullName = firstNames + " " + lastName;
         getNodes().resumeName.textContent = fullName;
       });
     }
@@ -15,9 +21,9 @@ const responsiveTyping = function () {
 
     const __forLocation = function (input) {
       input.addEventListener("input", () => {
-        let country = getNodes().personalCountryInput.value;
-        let city = getNodes().personalCityInput.value;
-        let location = country + ", " + city;
+        const country = getNodes().personalCountryInput.value;
+        const city = getNodes().personalCityInput.value;
+        const location = country + ", " + city;
         getNodes().resumePersonalLocation.textContent = location;
       });
     };
@@ -28,24 +34,20 @@ const responsiveTyping = function () {
 
     // }
 
-    const __forRest = function (inputTake, inputGive) {
-      inputGive.addEventListener("input", () => {
-        inputTake.textContent = inputGive.value;
-      });
-    };
-    __forRest(getNodes().resumePersonalMail, getNodes().emailInput);
-    __forRest(getNodes().resumePersonalPhone, getNodes().phoneInput);
-    __forRest(getNodes().resumePersonalAddress, getNodes().addressInput);
+    __forIndepInput(getNodes().resumePersonalMail, getNodes().emailInput);
+    __forIndepInput(getNodes().resumePersonalPhone, getNodes().phoneInput);
+    __forIndepInput(getNodes().resumePersonalAddress, getNodes().addressInput);
   })();
 
+  const __forLocation = function (input, inputCountry, inputCity, resume) {
+    input.addEventListener("input", () => {
+      const country = inputCountry.value;
+      const city = inputCity.value;
+      resume.textContent = country + " ⚫ " + city;
+    });
+  };
+
   const __forEducation = (function () {
-    const __forLocation = function (input, inputCountry, inputCity, resume) {
-      input.addEventListener("input", () => {
-        let country = inputCountry.value;
-        let city = inputCity.value;
-        resume.textContent = country + " ⚫ " + city;
-      });
-    };
     __forLocation(
       getNodes().firstEduCountry,
       getNodes().firstEduCountry,
@@ -74,10 +76,10 @@ const responsiveTyping = function () {
 
     const __forDate = function (input, initialDate, finalDate, resumeDate) {
       input.addEventListener("input", () => {
-        let startDate = new Date(initialDate.value);
-        let endDate = new Date(finalDate.value);
-        let startYear = startDate.getFullYear() || "";
-        let endYear = endDate.getFullYear() || "";
+        const startDate = new Date(initialDate.value);
+        const endDate = new Date(finalDate.value);
+        const startYear = startDate.getFullYear() || "";
+        const endYear = endDate.getFullYear() || "";
         resumeDate.textContent = startYear + " - " + endYear;
       });
     };
@@ -107,18 +109,49 @@ const responsiveTyping = function () {
       getNodes().resumeLastEduDate,
     );
 
-    const __forRest = function (inputTake, inputGive) {
-      inputGive.addEventListener("input", () => {
-        inputTake.textContent = inputGive.value;
-      });
-    };
-    __forRest(getNodes().resumeFirstEduSchool, getNodes().firstEduSchool);
-    __forRest(getNodes().resumeFirstEduDegree, getNodes().firstEduDegree);
-    __forRest(getNodes().resumeFirstEduHonors, getNodes().firstEduHonors);
+    __forIndepInput(getNodes().resumeFirstEduSchool, getNodes().firstEduSchool);
+    __forIndepInput(getNodes().resumeFirstEduDegree, getNodes().firstEduDegree);
+    __forIndepInput(getNodes().resumeFirstEduHonors, getNodes().firstEduHonors);
 
-    __forRest(getNodes().resumeLastEduSchool, getNodes().lastEduSchool);
-    __forRest(getNodes().resumeLastEduDegree, getNodes().lastEduDegree);
-    __forRest(getNodes().resumeLastEduHonors, getNodes().lastEduHonors);
+    __forIndepInput(getNodes().resumeLastEduSchool, getNodes().lastEduSchool);
+    __forIndepInput(getNodes().resumeLastEduDegree, getNodes().lastEduDegree);
+    __forIndepInput(getNodes().resumeLastEduHonors, getNodes().lastEduHonors);
+  })();
+
+  const __forProfession = (function () {
+    __forLocation(
+      getNodes().firstProCountry,
+      getNodes().firstProCountry,
+      getNodes().firstProCity,
+      getNodes().resumeFirstProLocation,
+    );
+    __forLocation(
+      getNodes().firstProCity,
+      getNodes().firstProCountry,
+      getNodes().firstProCity,
+      getNodes().resumeFirstProLocation,
+    );
+
+    __forLocation(
+      getNodes().lastProCountry,
+      getNodes().lastProCountry,
+      getNodes().lastProCity,
+      getNodes().resumelastProLocation,
+    );
+    __forLocation(
+      getNodes().lastProCity,
+      getNodes().lastProCountry,
+      getNodes().lastProCity,
+      getNodes().resumelastProLocation,
+    );
+
+    __forIndepInput(getNodes().resumeFirstProCompany, getNodes().firstProCompany);
+    __forIndepInput(getNodes().resumeFirstProPosition, getNodes().firstProPosition);
+    __forIndepInput(getNodes().resumeFirstProDescription, getNodes().firstProDescription);
+
+    __forIndepInput(getNodes().resumelastProCompany, getNodes().lastProCompany);
+    __forIndepInput(getNodes().resumelastProPosition, getNodes().lastProPosition);
+    __forIndepInput(getNodes().resumelastProDescription, getNodes().lastProDescription);
   })();
 };
 
