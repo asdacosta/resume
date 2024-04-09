6d5f66656be4e88073ce6b2ee20ce8ac503f4979
asdacosta <asdacosta18@gmail.com>
2024-04-09 07:44:26 +0000

    Add input listeners to first education box in Resume

--- src/components/responsiveTyping.js
+++ src/components/responsiveTyping.js
@@ -24,6 +24,10 @@ const responsiveTyping = function () {
     __forLocation(getNodes().personalCountryInput);
     __forLocation(getNodes().personalCityInput);
 
+    // const __forProfilePic = function () {
+
+    // }
+
     const __forRest = function (inputTake, inputGive) {
       inputGive.addEventListener("input", () => {
         inputTake.textContent = inputGive.value;
@@ -32,6 +36,41 @@ const responsiveTyping = function () {
     __forRest(getNodes().resumePersonalMail, getNodes().emailInput);
     __forRest(getNodes().resumePersonalPhone, getNodes().phoneInput);
     __forRest(getNodes().resumePersonalAddress, getNodes().addressInput);
+  })();
+
+  const __forEducation = (function () {
+    const __forLocation = function (input) {
+      input.addEventListener("input", () => {
+        let country = getNodes().firstEduCountry.value;
+        let city = getNodes().firstEduCity.value;
+        let location = country + " ⚫ " + city;
+        getNodes().resumeFirstEduLocation.textContent = location;
+      });
+    };
+    __forLocation(getNodes().firstEduCountry);
+    __forLocation(getNodes().firstEduCity);
+
+    const __forDate = function (input) {
+      input.addEventListener("input", () => {
+        let startDate = new Date(getNodes().firstEduStartDate.value);
+        let endDate = new Date(getNodes().firstEduEndDate.value);
+        let startYear = startDate.getFullYear() || "";
+        let endYear = endDate.getFullYear() || "";
+        let year = startYear + " - " + endYear;
+        getNodes().resumeFirstEduDate.textContent = year;
+      });
+    };
+    __forDate(getNodes().firstEduStartDate);
+    __forDate(getNodes().firstEduEndDate);
+
+    const __forRest = function (inputTake, inputGive) {
+      inputGive.addEventListener("input", () => {
+        inputTake.textContent = inputGive.value;
+      });
+    };
+    __forRest(getNodes().resumeFirstEduSchool, getNodes().firstEduSchool);
+    __forRest(getNodes().resumeFirstEduDegree, getNodes().firstEduDegree);
+    __forRest(getNodes().resumeFirstEduHonors, getNodes().firstEduHonors);
   })();
 };
 
